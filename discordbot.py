d846fd47a042fdee9402a5c236a7ed9bc2e72021
Lee Liak Ghee <liakify@gmail.com>
2021-07-17 14:25:02 +0800

    first commit

---
+++ discordbot.py
@@ -0,0 +1,34 @@
+# Imports
+import discord
+from discord.ext import commands
+
+# Credentials
+TOKEN = 'INSERT TOKEN HERE'
+
+# Create bot
+client = commands.Bot(command_prefix='!')
+
+# Startup Information
+@client.event
+async def on_ready():
+    print('Connected to bot: {}'.format(client.user.name))
+    print('Bot ID: {}'.format(client.user.id))
+
+# Command
+@client.command()
+async def altcaps(ctx):
+    content = ctx.message.content.split(None, 1)[1]
+    res = ""
+    lower = True
+    for c in content:
+        newc = c
+        if c.isalpha():
+            if lower:
+                newc = c.lower()
+            else:
+                newc = c.upper()
+            lower = not lower
+        res += newc
+    await ctx.send(content=res)
+
+client.run(TOKEN)
